de3d08327f85aea0c004cf46dd9dfb09e2b3bb4d
Billy <billybright1992@gmail.com>
2020-02-13 22:07:23 -0800

    front-end todo list

---
+++ build/precache-manifest.47135a76311a39742bfef49ce59bcb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9604dbfaa41870907700c3a1f23b42fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53094206d65dffc0dcd3",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "be103537709e9c8cf8ff",
+    "url": "/static/js/2.a35b971a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a35b971a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53094206d65dffc0dcd3",
+    "url": "/static/js/main.b6b7600b.chunk.js"
+  },
+  {
+    "revision": "757ad8be914975125e00",
+    "url": "/static/js/runtime-main.96eb0075.js"
+  }
+]);
